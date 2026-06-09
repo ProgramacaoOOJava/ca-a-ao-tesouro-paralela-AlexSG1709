358d3c56b44692d58248834fbd715f76d1efa29e
AlexSG1709 <programadoralex.github@gmail.com>
2026-06-09 09:50:26 -0400

    Create Tarefa.java

---
+++ Tarefa.java
@@ -0,0 +1,13 @@
+package br.com.cacaaotesouro;
+
+public class Tarefa {
+    private final String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
